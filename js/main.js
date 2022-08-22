119bf9cf3acbc79c7ddd2400e86e89294e392f2a
haquynh91 <haquynh91@gmail.com>
2022-08-21 22:16:55 -0400

    update

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  $(".product-slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: true,
+    draggable: false,
+    prevArrow: `<button type='button' class='slick-prev slick-arrow'><i class="fa-solid fa-chevron-left slick-arrow-btn" style="color: #ADB5BD;"></i></button>`,
+    nextArrow: `<button type='button' class='slick-next slick-arrow'><i class="fa-solid fa-chevron-right slick-arrow-btn" style="color: #ADB5BD;"></button>`,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          arrows: false,
+          infinite: false,
+        },
+      },
+    ],
+  });
+});
---
+++ js/shop.js
@@ -0,0 +1,24 @@
+const products = [
+    {
+        product-name: 'Product 1',
+        original-price: '$133.99',
+        discount-price: '$133.99',
+        sale-off: '20%',
+        product-img: img01,
+    },
+
+    {  title: '03 DEV (.NET Developer hoặc Database)',
+      company: 'Ngân Hàng TMCP Xăng Dầu Petrolimex',
+      salary: {
+        min: 0,
+        max: 0,
+        currency: 'vnd',
+        negotiation: false,
+        compatitive: true,
+      },
+      location: 'ha-noi',
+      benefits: ['bao-hiem', 'du-lich', 'thuong'],
+      created: '17/06/2022',
+      contract: 'chinh-thuc',
+      level: 'nhan-vien',
+    },
